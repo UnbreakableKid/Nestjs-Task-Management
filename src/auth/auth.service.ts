336c16c0dc107015703729abacb6dd4438b07630
João Rafael Pacheco <jrpacheco.dev@gmail.com>
2021-04-25 17:33:18 +0100

    stopping for today, JWT video 10

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,22 +1,32 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dtos/auth-credentials.dto';
+import { JwTPayload } from './jwt-payload.interface';
 import { UserRepository } from './user.repository';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository) private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto) {
     return this.userRepository.signUp(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto) {
-    const rs = await this.userRepository.validateUserPassword(
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    const username = await this.userRepository.validateUserPassword(
       authCredentialsDto,
     );
-    if (!rs) throw new UnauthorizedException('Invalid credentials');
+    if (!username) throw new UnauthorizedException('Invalid credentials');
+
+    const payload: JwTPayload = { username };
+    const accessToken = await this.jwtService.signAsync(payload);
+
+    return { accessToken: accessToken };
   }
 }
