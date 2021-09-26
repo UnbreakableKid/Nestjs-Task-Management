d648bbd65517a560e65fcacc9755167406c9c462
João Rafael Pacheco <29483407+UnbreakableKid@users.noreply.github.com>
2021-09-26 18:49:18 +0100

    Update typeorm.config.ts

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,7 +5,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   host: 'localhost',
   port: 5432,
   username: 'postgres',
-  password: 'sandyravage',
+  password: 'password',
   database: 'taskmanagement',
   entities: [__dirname + '/../**/*.entity.js'],
   synchronize: true,
